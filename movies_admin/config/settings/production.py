a173974f8dfb65de2a43b1607220dedbeb896b8f
Ruslan <djvorf@gmail.com>
2021-02-19 22:12:04 +0600

    Add postgres in django app

--- movies_admin/config/settings/production.py
+++ movies_admin/config/settings/production.py
@@ -1,1 +1,12 @@
 from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
